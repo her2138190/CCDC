c812cdb8ab81b22210ae1a64c2c2e78a8b231445
her2138190 <55335566+her2138190@users.noreply.github.com>
2019-10-24 17:25:42 -0600

    Update wazuhsetup.sh

--- wazuhsetup.sh
+++ wazuhsetup.sh
@@ -16,6 +16,20 @@ dnf install nodejs
 dnf install wazuh-api
 
 
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cat > /etc/yum.repos.d/elastic.repo << EOF
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+
+
+
 dnf install filebeat-7.3.2
 curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/v3.10.2/extensions/filebeat/7.x/filebeat.yml
 chmod go+r /etc/filebeat/filebeat.yml
