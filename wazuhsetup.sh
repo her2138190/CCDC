904288b9a43f286d84978d226312a6000da67d03
her2138190 <55335566+her2138190@users.noreply.github.com>
2019-10-24 14:26:05 -0700

    Update wazuhsetup.sh

--- wazuhsetup.sh
+++ wazuhsetup.sh
@@ -14,17 +14,7 @@ service wazuh-manager status
 systemctl status wazuh-manager
 dnf install nodejs
 dnf install wazuh-api
-rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
-cat > /etc/yum.repos.d/elastic.repo << EOF
-[elasticsearch-7.x]
-name=Elasticsearch repository for 7.x packages
-baseurl=https://artifacts.elastic.co/packages/7.x/yum
-gpgcheck=1
-gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-enabled=1
-autorefresh=1
-type=rpm-md
-EOF
+
 
 dnf install filebeat-7.3.2
 curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/v3.10.2/extensions/filebeat/7.x/filebeat.yml
@@ -32,7 +22,7 @@ chmod go+r /etc/filebeat/filebeat.yml
 curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/v3.10.2/extensions/elasticsearch/7.x/wazuh-template.json
 chmod go+r /etc/filebeat/wazuh-template.json
 curl -s https://packages.wazuh.com/3.x/filebeat/wazuh-filebeat-0.1.tar.gz | sudo tar -xvz -C /usr/share/filebeat/module
-#now edit /etc/filebeat.yml and add your internal ip to the config file....
+echo "--------now edit /etc/filebeat.yml and add your internal ip to the config file... and cat this script and read after this line...-----------"
 
 #Enable and start the Filebeat service:
 
