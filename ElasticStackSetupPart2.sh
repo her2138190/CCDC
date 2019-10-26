74dbff0559828e0b5ae3711f6ea0557439426b4e
her2138190 <55335566+her2138190@users.noreply.github.com>
2019-10-26 08:26:38 -0700

    Update ElasticStackSetupPart2.sh

--- ElasticStackSetupPart2.sh
+++ ElasticStackSetupPart2.sh
@@ -2,7 +2,7 @@ yum install kibana-7.3.2
 
 sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/wazuhapp/wazuhapp-3.10.2_7.3.2.zip
 
-echo"--phase two almost complete.. cat this shell script and follow the rest of the instructions....---"
+echo "--phase two almost complete.. cat this shell script and follow the rest of the instructions....---"
 #Kibana will only listen on the loopback interface (localhost) by default, which means that it can be only accessed from the same machine. To access Kibana from the outside make it listen on its network IP by editing the file /etc/kibana/kibana.yml, uncomment the setting server.host, and change the value to:
 
   #  server.host: "<kibana_ip>"
